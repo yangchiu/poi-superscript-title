dd21a1c87031a04b12402fc3ad102eda1b552af7
yangchiu <alichchiu@gmail.com>
2019-04-11 17:28:04 +0800

    initial commit

---
+++ src/DocumentReader.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class DocumentReader {
+	
+	protected int contentFontSize;
+	protected HashMap<Integer, Integer> mapOfFontSize = new HashMap<Integer, Integer>();
+	protected final String openSup = "<sup>";
+	protected final String closedSup = "</sup>";
+	protected final String openTitle = "<title>";
+	protected final String closedTitle = "</title>";
+
+}

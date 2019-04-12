98e4b0316db5741ef0064b5d0d0cb2ec01957a1e
yangchiu <alichchiu@gmail.com>
2019-04-12 13:54:20 +0800

    doc reader first commit

--- src/com/yangchiu/poi/doc/DocReader.java
+++ src/com/yangchiu/poi/doc/DocReader.java
@@ -4,6 +4,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.extractor.WordExtractor;
+import org.apache.poi.hwpf.usermodel.CharacterRun;
 import org.apache.poi.hwpf.usermodel.Range;
 
 import com.yangchiu.poi.common.DocumentReader;
@@ -13,14 +15,33 @@ public class DocReader extends DocumentReader {
     public static void main(String[] args) {
         
         try {
-            FileInputStream fis = new FileInputStream("test_superscript.doc");
+            FileInputStream fis = new FileInputStream("test_superscript3.doc");
             HWPFDocument doc = new HWPFDocument(fis);
             Range range = doc.getRange();
             
-            System.out.println(range.text());
+            int numOfRuns = range.numCharacterRuns();
+            for(int i = 0;i < numOfRuns;i++) {
+                CharacterRun rn = range.getCharacterRun(i);
+                String text = rn.text();
+                int fontSize = rn.getFontSize();
+                int vertical = rn.getVerticalOffset();
+                short index1 = rn.getSubSuperScriptIndex();
+                
+                // if ( characterRun.getSubSuperScriptIndex() == 1 ) ==> super
+                // if ( characterRun.getSubSuperScriptIndex() == 2 ) ==> sub
+                
+                System.out.println("[" + i + "] " + text);
+                System.out.println("font size = " + fontSize + ", vertical offset = " + vertical + ", index = " + index1);
+            }
             
+            //System.out.println(range.text());
             
+            WordExtractor ex = new WordExtractor(doc);
+            String text = ex.getText();
             
+           // System.out.println(text);
+            
+            ex.close();
             doc.close();
 
         } catch (IOException e) {
